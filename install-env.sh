232943e4f2850e761b43edb9b2fb3eda21810cce
Wei Dong <wdong@wdong.org>
2018-09-20 09:36:16 -0400

    update

--- install-env.sh
+++ install-env.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-if false
-then
 wget https://nodejs.org/dist/v8.12.0/node-v8.12.0-linux-x64.tar.xz
 tar xf node-v8.12.0-linux-x64.tar.xz
 mv node-v8.12.0-linux-x64 env
 virtualenv --python=`which python3` env
-fi
 
 . env/bin/activate
-#pip3 install django
-#pip3 install numpy
+pip3 install django
+pip3 install numpy
 pip3 install simplejson h5py scipy
